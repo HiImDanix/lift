b2fa127ca4610d4469eb73bee15fdc2cad2491a2
attilaUCN <10407547@ucn.dk>
2022-11-29 13:45:26 +0100

    model Question  created

--- desktop-client/desktop-client/ModelLayer/Question.cs
+++ desktop-client/desktop-client/ModelLayer/Question.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace desktop_client.ModelLayer
 {
-    internal class Question
+    public class Question
     {
+        public string ImagePath { get; set; }
+        public string QuestionText { get; set; }
+        public string Category { get; set; }
+        public string Answer { get; set; }
+        public Question() { }
+
+        public Question(string imagePath, string question, string category, string answer)
+        {
+            ImagePath = imagePath;
+            QuestionText = question;
+            Category = category;
+            Answer = answer;
+        }
+
+
+       
     }
 }
