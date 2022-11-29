192e9b65ceb389b467ce8edf9f3d751354258c7d
Daniels Kanepe <danixkanepe@gmail.com>
2022-11-29 12:13:54 +0100

    Validation

--- server/server/Models/Player.cs
+++ server/server/Models/Player.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace GuessingGame.models;
@@ -5,7 +6,12 @@ namespace GuessingGame.models;
 public class Player
 {
     public int Id { get; set; }
+    [Required]
     public string Session { get; set; }
+    [MinLength(3),
+     MaxLength(16),
+     Required,
+     RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Player name can only contain letters and numbers")]
     public string DisplayName { get; set; }
     [JsonIgnore]
     public virtual Room? Room { get; set; }
