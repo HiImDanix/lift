d050fb85484b1dbe4751abdf6b1395d8a30aa965
MartinSchneid <10407553@ucn.dk>
2022-11-29 13:53:06 +0100

    Question adding setup on client side

--- desktop-client/desktop-client/ControlLayer/QuestionController.cs
+++ desktop-client/desktop-client/ControlLayer/QuestionController.cs
@@ -9,10 +9,11 @@ namespace desktop_client.ControlLayer
 {
     internal class QuestionController
     {
-        public async Task<int> SaveQuestion()
+        public async Task<int> SaveQuestion(string imagePath, string question, string category, string answer)
         {
             int insertId = -1;
-            
+            Question newQuestion = new Question(imagePath, question, category, answer);
+            return insertId;
 
             throw new NotImplementedException();
         }
---
+++ desktop-client/desktop-client/ServiceLayer/QuestionService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using desktop_client.ModelLayer;
+using Newtonsoft.Json;
+
+namespace desktop_client.ServiceLayer
+{
+    public class QuestionService
+    {
+        static readonly string restUrl = "https://localhost:7246/Questions";
+        readonly HttpClient _httpClient;
+
+        public HttpStatusCode CurrentHttpStatusCode { get; set; }
+
+        public QuestionService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<int> SaveQuestion(Question newQuestion)
+        {
+            int insertedQuestionId;
+            string useRestUrl = restUrl;
+            var uri = new Uri(string.Format(useRestUrl));
+
+            try
+            {
+                var json = JsonConvert.SerializeObject(newQuestion);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                response = await _httpClient.PostAsync(uri, content);
+                CurrentHttpStatusCode = response.StatusCode;
+                string resultIdString = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    Int32.TryParse(resultIdString, out insertedQuestionId);
+                }
+                else
+                {
+                    insertedQuestionId = -2;
+                }
+            }
+            catch
+            {
+                insertedQuestionId = -3;
+            }
+            return insertedQuestionId;
+        }
+    }
+}
