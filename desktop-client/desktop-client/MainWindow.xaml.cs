4eaac22df18f220c272db2d80de7b80080abcb28
Katarína Brezániová <10407558@ucn.dk>
2022-11-29 11:28:16 +0100

    naming  items

--- desktop-client/desktop-client/MainWindow.xaml.cs
+++ desktop-client/desktop-client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Shell;
 
 namespace desktop_client
 {
@@ -22,7 +23,7 @@ namespace desktop_client
     {
         public MainWindow()
         {
-            InitializeComponent();
+          
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
