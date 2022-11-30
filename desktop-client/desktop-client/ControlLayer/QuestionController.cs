0cce18443078a1b3f8cd074f9d9b2c22c32f8488
MartinSchneid <10407553@ucn.dk>
2022-11-30 17:21:26 +0100

    Fixed silly mistake of not actually executing the method

--- desktop-client/desktop-client/ControlLayer/QuestionController.cs
+++ desktop-client/desktop-client/ControlLayer/QuestionController.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using desktop_client.ModelLayer;
+using desktop_client.ServiceLayer;
 
 namespace desktop_client.ControlLayer
 {
     internal class QuestionController
     {
+        QuestionService _qService;
+
+        public QuestionController()
+        {
+            _qService = new QuestionService();
+        }
         public async Task<int> SaveQuestion(string imagePath, string question, string category, string answer)
         {
             int insertId = -1;
             Question newQuestion = new Question(imagePath, question, category, answer);
-            return insertId;
 
-            throw new NotImplementedException();
+            insertId = await _qService.SaveQuestion(newQuestion);
+            return insertId;
         }
     }
 }
