c28d4de9cc00088ee2787431daa75b6e07e95938
Katarína Brezániová <93518966+mazonk@users.noreply.github.com>
2022-12-07 12:54:40 +0100

    Delete Server/Server directory

--- Server/Server/Repositories/IDesktopAuthRepository.cs
+++
@@ -1,8 +0,0 @@
-using GuessingGame.Models;
-
-namespace GuessingGame.Repositories;
-
-public interface IDesktopAuthRepository
-{
-    Administrator? GetByEmail(string email);
-}
--- Server/Server/Services/IDekstopAuthService.cs
+++
@@ -1,9 +0,0 @@
-using GuessingGame.DTO.responses;
-using GuessingGame.models;
-
-namespace GuessingGame.Services;
-
-public interface IDesktopAuthService
-{
-    bool Login(string email, string password);
-}
