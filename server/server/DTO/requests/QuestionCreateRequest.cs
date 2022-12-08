2e4606834b6cceb47f5124b0502ba87c647f0196
Daniels Kanepe <danixkanepe@gmail.com>
2022-12-08 14:39:38 +0100

    dto validation

--- server/server/DTO/requests/QuestionCreateRequest.cs
+++ server/server/DTO/requests/QuestionCreateRequest.cs
@@ -1,9 +1,17 @@
-﻿namespace GuessingGame.DTO.requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuessingGame.DTO.requests;
 public class QuestionCreateRequest
 {
+    [Required]
+    [Url]
+    
     public string ImagePath { get; set; }
+    [Required]
     public string QuestionText { get; set; }
+    [Required]
     public string Category { get; set; }
     public byte[] RowVer { get; set; }
+    [Required]
     public List<AnswerNoId> Answers { get; set; }
 }
--- server/server/DTO/responses/AnswerNoId.cs
+++ server/server/DTO/responses/AnswerNoId.cs
@@ -1,8 +1,13 @@
 // Rewrite as a class
+
+using System.ComponentModel.DataAnnotations;
+
 namespace GuessingGame.DTO.requests;
 
 public class AnswerNoId
 {
+    [Required]
     public string AnswerText { get; set; }
+    [Required]
     public bool IsCorrect { get; set; }
 }
