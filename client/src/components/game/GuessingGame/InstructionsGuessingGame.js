6065a222b5b4c542ec5f8249c2f7cfbc78779cde
Daniels Kanepe <danixkanepe@gmail.com>
2022-12-09 10:41:41 +0100

    Red countdown for visibility

--- client/src/components/game/GuessingGame/InstructionsGuessingGame.js
+++ client/src/components/game/GuessingGame/InstructionsGuessingGame.js
@@ -11,8 +11,8 @@ function InstructionsGuessingGame(props) {
             <h1 className="text-uppercase fw-bold text-center">Instructions</h1>
             <p className="text-center">You will be shown a logo. You must guess the name of the company that owns the logo.</p>
             <p className="text-center">You will have {props.timeLimit} seconds to guess the name of the company.</p>
-            <p className="text-center">The game will start in <Countdown date={props.startTime}
-                                                                         renderer={SecondsCountdownRenderer} /> seconds
+            <p className="text-center">The game will start in <span className={"text-danger"}><Countdown date={props.startTime}
+                                                                         renderer={SecondsCountdownRenderer} /></span> seconds
             </p>
             <p className="text-center">Have fun!</p>
         </>
